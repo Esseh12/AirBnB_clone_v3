a35e35dc8099d72c0ec223e65dbab4619f2c3033
imoleFolio <dremkay71@gmail.com>
2024-05-27 15:42:51 +0100

    task 6: all rest api done for state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,12 +2,13 @@
 """ contains routes for the api"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort
-from werkzeug.exceptions import NotFound
+from flask import jsonify, abort, request
+from werkzeug.exceptions import NotFound, BadRequest
 from models import storage
+from models.state import State
 
 
-@app_views.route('/states', strict_slashes=False, methods=['GET'])
+@app_views.get('/states', strict_slashes=False)
 def states():
     """return api status"""
     st = storage.all('State').values()
@@ -15,14 +16,14 @@ def states():
     return jsonify(all_states)
 
 
-# @app_views.route('/states/<state_id>')
-# def get_state(state_id):
-#     """return api status"""
-#     st = storage.get('State', state_id)
-#     if st:
-#         return jsonify(st.to_dict())
-#     else:
-#         raise NotFound()
+@app_views.get('/states/<state_id>')
+def get_state(state_id):
+    """return api status"""
+    st = storage.get('State', state_id)
+    if st:
+        return jsonify(st.to_dict())
+    else:
+        raise NotFound()
 
 
 @app_views.delete('/states/<state_id>', strict_slashes=False)
@@ -33,3 +34,32 @@ def state_delete(state_id):
         return jsonify({}), 200
     else:
         abort(404)
+
+
+@app_views.post('/states', strict_slashes=False)
+def create_state():
+    data = request.get_json()
+    if not isinstance(data, dict):
+        raise BadRequest(description='Not a JSON')
+    if 'name' not in data:
+        raise BadRequest(description="Missing name")
+    obj = State(**data)
+    obj.save()
+    return jsonify(obj.to_dict()), 201
+
+
+@app_views.put('/states/<state_id>', strict_slashes=False)
+def update_state(state_id):
+    data = request.get_json()
+    if not isinstance(data, dict):
+        raise BadRequest(description='Not a JSON')
+    if 'name' not in data:
+        raise BadRequest(description="Missing name")
+    st = storage.get('State', state_id)
+    if not st:
+        NotFound(404)
+    for k,v in data.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(st, k, v)
+    st.save()
+    return jsonify(st.to_dict()), 200
